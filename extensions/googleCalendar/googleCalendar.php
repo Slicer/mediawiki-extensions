2ee7245e729e198cfb1cfd853540d14671271d08
Adam Rankin <adam.rankin@gmail.com>
2015-11-24 18:49:06 -0500

    Resolving indentation

--- extensions/googleCalendar/googleCalendar.php
+++ extensions/googleCalendar/googleCalendar.php
@@ -11,20 +11,20 @@
 
 $wgExtensionFunctions[] = 'wfGoogleCalendar';
 $wgExtensionCredits['parserhook'][] = array(
-        'name' => 'Google Calendar',
-        'description' => 'Display Google Calendar',
-        'author' => 'Kasper Souren',
-        'url' => 'http://wiki.couchsurfing.com/en/Google_Calendar_MediaWiki_plugin'
+    'name' => 'Google Calendar',
+    'description' => 'Display Google Calendar',
+    'author' => 'Kasper Souren',
+    'url' => 'http://wiki.couchsurfing.com/en/Google_Calendar_MediaWiki_plugin'
 );
 
 function wfGoogleCalendar() {
-        global $wgParser;
-        $wgParser->setHook('googlecalendar', 'renderGoogleCalendar');
+    global $wgParser;
+    $wgParser->setHook('googlecalendar', 'renderGoogleCalendar');
 }
 
 # The callback function for converting the input text to HTML output
 function renderGoogleCalendar($input, array $args) {
-        $input = htmlspecialchars($input);
+    $input = htmlspecialchars($input);
 
     $width = 480;
     if( isset( $args['width'] ) && $args['width'] ) {
@@ -41,8 +41,8 @@ function renderGoogleCalendar($input, array $args) {
       $title = $args['title'];
     }
 
-        $output = '<iframe src="http://www.google.com/calendar/embed?src='.$input.'&title='.$title.'&chrome=NAVIGATION&height='.$height.'&epr=4" style=" border-width:0 " width="'.$width.'" frameborder="0" height="'.$height.'"></iframe>';
+    $output = '<iframe src="http://www.google.com/calendar/embed?src='.$input.'&title='.$title.'&chrome=NAVIGATION&height='.$height.'&epr=4" style=" border-width:0 " width="'.$width.'" frameborder="0" height="'.$height.'"></iframe>';
 
-        return $output;
+    return $output;
 }
 ?>
